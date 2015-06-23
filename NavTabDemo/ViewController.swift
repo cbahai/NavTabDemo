2b09d2440ba423e9f9cb85963a73d72490a9fe19
FengYongHai <2206728426@qq.com>
2015-06-23 11:22:34 +0800

    Initial Commit

---
+++ NavTabDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavTabDemo
+//
+//  Created by 冯勇海 on 15/6/23.
+//  Copyright © 2015年 Chai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
